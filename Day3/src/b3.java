5b7ab13314c727d5c8d6ae5521e736d509549dca
Hoàng Trần Hảo <tranhaockpt@gmail.com>
2016-06-26 04:06:39 +0700

    Update b3.java

--- Day3/src/b3.java
+++ Day3/src/b3.java
@@ -64,8 +64,14 @@ public class b3 {
                 m=12;
                 break;
         }
-        j=(year-1)/100;
-        k=(year-1)%100;
+        if (month==1||month==2) {
+            j = (year - 1) / 100;
+            k = (year - 1) % 100;
+        }
+        else {
+            j = year / 100;
+            k = year % 100;
+        }
         h=(q + (26*(m+1))/10 + k + k/4 + j/4 + 5*j)%7;
         switch (h) {
             case 0:
